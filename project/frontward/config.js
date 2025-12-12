eba71bfe58c3b03f801443688da143fecc63dad4
wyt178 <u202314515@hust.edu.cn>
2025-12-12 16:40:33 +0800

    Update config.js

--- project/frontward/config.js
+++ project/frontward/config.js
@@ -1,1 +1,5 @@
-
+// config.js - YOLO服务器配置
+window.SERVER_IP = "http://localhost:8000"
+// 或者如果您的后端在不同端口，例如：
+// window.SERVER_IP = "http://localhost:5000";
+// window.SERVER_IP = "http://192.168.1.100:8000";
